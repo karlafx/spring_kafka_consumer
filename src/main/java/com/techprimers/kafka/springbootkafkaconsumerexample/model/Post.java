5e8504388f118a50db255677ae6ef0cdfe069a77
freistadt <flo.sitter@gmx.at>
2021-06-23 17:02:19 +0200

    Added Posting Class
    --> Troubles with Dependencies

---
+++ src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/model/Post.java
@@ -0,0 +1,56 @@
+package com.techprimers.kafka.springbootkafkaproducerexample.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Post implements Serializable {
+
+    private String postingId;
+    private String userId;
+    private String content;
+    private String emotion;
+    private Date creationDate;
+
+    public Post(){
+    }
+
+    public String getPostingId() {
+        return postingId;
+    }
+
+    public void setPostingId(String postingId) {
+        this.postingId = postingId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getEmotion() {
+        return emotion;
+    }
+
+    public void setEmotion(String emotion) {
+        this.emotion = emotion;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
